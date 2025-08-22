6a038fdc30819f067b2a80f3ec9de9df009e1bd6
kangmeng3 <kangmeng3@jd.com>
2025-08-22 16:13:50 +0800

    refactor: refactor InstanceMgr.

--- xllm_service/rpc_service/instance_mgr.cpp
+++ xllm_service/rpc_service/instance_mgr.cpp
@@ -20,9 +20,11 @@ limitations under the License.
 
 #include <chrono>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 #include "common/types.h"
 #include "common/utils.h"
+
 namespace xllm_service {
 
 // magic number, TODO: move to config file or env var
@@ -34,236 +36,59 @@ static std::unordered_map<InstanceType, std::string> ETCD_KEYS_PREFIX_MAP = {
 };
 static std::string ETCD_ALL_KEYS_PREFIX = "XLLM:";
 static std::string DEFAULT_DISAGG_PD_POLICY = "RR";
+static std::string ETCD_LOADMETRICS_PREFIX = "XLLM:LOADMETRICS:";
 
-InstanceMgr::InstanceMgr(const RpcServiceConfig& config) : config_(config) {
-  if (config.etcd_addr.empty()) {
-    LOG(INFO) << "Disable etcd meta server";
-    use_etcd_ = false;
-  } else {
-    LOG(INFO) << "Connect to etcd meta server: " << config.etcd_addr;
-    use_etcd_ = true;
-    etcd_client_ = std::make_unique<EtcdClient>(config.etcd_addr);
+InstanceMgr::InstanceMgr(const std::shared_ptr<EtcdClient>& etcd_client,
+                         const HttpServiceConfig& config,
+                         const bool is_master_service)
+    : http_config_(config),
+      is_master_service_(is_master_service),
+      etcd_client_(etcd_client) {
+  auto handle_instance_metainfo =
+      std::bind(&InstanceMgr::handle_instance_metainfo_watch,
+                this,
+                std::placeholders::_1,
+                std::placeholders::_2);
+  for (auto& it : ETCD_KEYS_PREFIX_MAP) {
+    etcd_client_->add_watch(it.second, handle_instance_metainfo);
+  }
+  if (!is_master_service_) {
+    auto handle_load_metrics =
+        std::bind(&InstanceMgr::handle_load_metrics_watch,
+                  this,
+                  std::placeholders::_1,
+                  std::placeholders::_2);
+    etcd_client_->add_watch(ETCD_LOADMETRICS_PREFIX, handle_load_metrics);
   }
 
-  internal_init();
+  init();
 }
 
-void InstanceMgr::internal_init() {
-  std::string pd_policy = config_.disagg_pd_policy;
-  if (config_.disagg_pd_policy.empty()) {
-    LOG(WARNING) << "Not specify diasgg pd policy, use `RR` policy as default.";
-    pd_policy = DEFAULT_DISAGG_PD_POLICY;
-  }
-  if (pd_policy == "RR") {
-    disagg_pd_policy_ = std::make_unique<RoundRobinDisaggPdPolicy>();
-  } else {
-    LOG(FATAL) << "Not supported diasgg pd policy: " << pd_policy;
-    return;
-  }
-
-  heartbeat_thread_ = std::make_unique<std::thread>(
-      &InstanceMgr::detect_disconnected_instances, this);
-}
-
-InstanceMgr::~InstanceMgr() {
-  exited_ = true;
-  if (heartbeat_thread_) {
-    heartbeat_thread_->join();
-  }
-}
-
-void InstanceMgr::detect_disconnected_instances() {
-  while (!exited_) {
-    std::this_thread::sleep_for(std::chrono::seconds(kDetectIntervals));
-    {
-      std::lock_guard<std::mutex> guard(inst_mutex_);
-      auto now = std::chrono::system_clock::now();
-      auto timestamp_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                              now.time_since_epoch())
-                              .count();
-      std::vector<std::string> disconnected_instances_name;
-      for (const auto& [name, info] : instances_) {
-        if (timestamp_ms - info.latest_timestamp > kDetectIntervals * 1000) {
-          LOG(WARNING) << "Instance maybe disconnected, instance_name: " << name
-                       << ", last heartbeat interval(s): "
-                       << (timestamp_ms - info.latest_timestamp) / 1000.0;
-          disconnected_instances_name.emplace_back(name);
-        }
-      }
-
-      // no instances disconnected, return
-      if (disconnected_instances_name.empty()) {
-        continue;
-      }
-
-      if (utils::enable_debug_log()) {
-        const auto instance_names =
-            absl::StrJoin(disconnected_instances_name, ", ");
-        LOG(WARNING) << "Detect disconnected instance, instance_name: "
-                     << instance_names;
-      }
-      // detele instance metainfo from etcd
-      delete_persistence_metainfo(disconnected_instances_name);
-      for (const auto& name : disconnected_instances_name) {
-        disagg_pd_policy_->remove_instance(name, instances_[name].type);
-        instances_.erase(name);
+void InstanceMgr::init() {
+  {
+    std::unique_lock<std::shared_mutex> lock(inst_mutex_);
+    for (auto& it : ETCD_KEYS_PREFIX_MAP) {
+      etcd_client_->get_prefix(it.second, &instances_);
+    }
+    LOG(INFO) << "Load instance info from etcd:" << instances_.size();
+    for (const auto& name : instances_) {
+      if (!create_channel(name.first)) {
+        zombie_nodes_.insert(name.first);
+        instances_.erase(name.first);
       }
     }
   }
-}
-
-ErrorCode InstanceMgr::register_instance(const std::string& instance_name) {
-  std::lock_guard<std::mutex> guard(inst_mutex_);
-  if (utils::enable_debug_log()) {
-    LOG(WARNING) << "Register instance, instance_name: " << instance_name;
-  }
-  if (instances_.find(instance_name) != instances_.end()) {
-    // update_instance_timestamp(instance_name);
-    LOG(ERROR) << "Instance is already registered, instance_name: "
-               << instance_name;
-    return ErrorCode::INSTANCE_EXISTED;
-  }
-
-  InstanceMetaInfo default_info(instance_name, "");
-  instances_[instance_name] = default_info;
-  disagg_pd_policy_->insert_instance(instance_name,
-                                     &(instances_[instance_name]));
-  // save instance metainfo to etcd
-  save_persistence_metainfo(default_info);
-  return ErrorCode::OK;
-}
-
-ErrorCode InstanceMgr::register_instance(const std::string& instance_name,
-                                         const InstanceMetaInfo& metainfo) {
-  std::lock_guard<std::mutex> guard(inst_mutex_);
-  if (utils::enable_debug_log()) {
-    LOG(WARNING) << "Register instance, instance_name: " << instance_name;
-  }
-  if (instances_.find(instance_name) != instances_.end()) {
-    // update_instance_timestamp(instance_name);
-    LOG(ERROR) << "Instance is already registered, instance_name: "
-               << instance_name;
-    return ErrorCode::INSTANCE_EXISTED;
-  }
-
-  instances_[instance_name] = metainfo;
-  disagg_pd_policy_->insert_instance(instance_name,
-                                     &(instances_[instance_name]));
-  // save instance metainfo to etcd
-  save_persistence_metainfo(metainfo);
-  return ErrorCode::OK;
-}
-
-ErrorCode InstanceMgr::update_instance_metainfo(
-    const std::string& instance_name,
-    const InstanceMetaInfo& metainfo) {
-  std::lock_guard<std::mutex> guard(inst_mutex_);
-  if (utils::enable_debug_log()) {
-    LOG(WARNING) << "Update instance metainfo, instance_name: "
-                 << instance_name;
-  }
-  if (instances_.find(instance_name) == instances_.end()) {
-    LOG(ERROR) << "Instance is not registered, instance_name: "
-               << instance_name;
-    return ErrorCode::INSTANCE_NOT_EXISTED;
-  }
-
-  instances_[instance_name] = metainfo;
-  update_instance_timestamp(instance_name);
-  disagg_pd_policy_->update_instance(instance_name,
-                                     &(instances_[instance_name]));
-  return ErrorCode::OK;
-}
-
-void InstanceMgr::save_persistence_metainfo(const InstanceMetaInfo& metainfo) {
-  if (!use_etcd_) {
-    return;
-  }
-  std::string key = ETCD_KEYS_PREFIX_MAP[metainfo.type] + metainfo.name;
-  InstanceIdentityInfo value;
-  value.instance_addr = metainfo.name;
-  value.rpc_addr = metainfo.rpc_address;
-  value.instance_type = static_cast<int8_t>(metainfo.type);
-  bool ok = etcd_client_->set(key, value);
-  if (!ok) {
-    LOG(ERROR) << "Save instance metainfo to etcd failed, key: " << key;
-    return;
-  }
-
-  if (utils::enable_debug_log()) {
-    InstanceIdentityInfo debug_value;
-    bool ok = etcd_client_->get(key, debug_value);
-    if (!ok) {
-      LOG(ERROR) << "Get instance metainfo from etcd failed, key: " << key;
-      return;
-    }
-    LOG(WARNING) << "Instance after put: " << debug_value.debug_string();
+  {
+    std::unique_lock<std::shared_mutex> lock(load_metric_mutex_);
+    etcd_client_->get_prefix(ETCD_LOADMETRICS_PREFIX, &load_metrics_);
   }
 }
 
-void InstanceMgr::delete_persistence_metainfo(
-    const std::vector<std::string>& instance_names) {
-  if (!use_etcd_ || instance_names.empty()) {
-    return;
-  }
-  // TODO: use batch delete later
-  for (const auto& name : instance_names) {
-    InstanceMetaInfo& metainfo = instances_[name];
-    std::string key = ETCD_KEYS_PREFIX_MAP[metainfo.type] + metainfo.name;
-    bool ok = etcd_client_->rm(key);
-    if (!ok) {
-      LOG(ERROR) << "Delete instance metainfo from etcd failed, key: " << key;
-    }
-  }
-
-  if (utils::enable_debug_log()) {
-    std::vector<InstanceIdentityInfo> debug_values;
-    bool ok = etcd_client_->get_prefix(ETCD_ALL_KEYS_PREFIX, debug_values);
-    if (!ok) {
-      LOG(ERROR) << "Get instance metainfo from etcd failed, key: "
-                 << ETCD_ALL_KEYS_PREFIX;
-      return;
-    }
-    std::string concat_debug_str;
-    for (const auto& v : debug_values) {
-      concat_debug_str += v.debug_string();
-      concat_debug_str += "\n";
-    }
-    LOG(WARNING) << "Instances after delete: " << concat_debug_str;
-  }
-}
-
-ErrorCode InstanceMgr::heartbeat(const std::string& instance_name) {
-  std::lock_guard<std::mutex> guard(inst_mutex_);
-  if (utils::enable_debug_log()) {
-    LOG(WARNING) << "Receive heartbeat, instance_name: " << instance_name;
-  }
-  if (instances_.find(instance_name) == instances_.end()) {
-    LOG(ERROR) << "Instance is not registered, instance_name: "
-               << instance_name;
-    return ErrorCode::INSTANCE_NOT_EXISTED;
-  }
-
-  update_instance_timestamp(instance_name);
-
-  return ErrorCode::OK;
-}
-
-void InstanceMgr::update_instance_timestamp(const std::string& inst_name) {
-  auto now = std::chrono::system_clock::now();
-  auto timestamp_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                          now.time_since_epoch())
-                          .count();
-  instances_[inst_name].latest_timestamp = timestamp_ms;
-}
-
-InstancesPair InstanceMgr::select_instances_pair(bool only_prefill) {
-  return disagg_pd_policy_->select_instances_pair(only_prefill);
-}
+InstanceMgr::~InstanceMgr() { exited_ = true; }
 
 InstanceMetaInfo InstanceMgr::get_instance_info(
     const std::string& instance_name) {
-  std::lock_guard<std::mutex> guard(inst_mutex_);
+  std::shared_lock<std::shared_mutex> lock(inst_mutex_);
   if (instances_.find(instance_name) == instances_.end()) {
     LOG(ERROR) << "Get instance info failed, instance is not registered, "
                   "instance_name: "
@@ -277,7 +102,7 @@ InstanceMetaInfo InstanceMgr::get_instance_info(
 std::vector<std::string> InstanceMgr::get_static_decode_list(
     const std::string& instance_name) {
   std::vector<std::string> decode_list;
-  std::lock_guard<std::mutex> guard(inst_mutex_);
+  std::shared_lock<std::shared_mutex> lock(inst_mutex_);
   for (auto& inst : instances_) {
     if (inst.second.type == InstanceType::DECODE) {
       decode_list.emplace_back(inst.second.name);
@@ -285,6 +110,240 @@ std::vector<std::string> InstanceMgr::get_static_decode_list(
   }
 
   return decode_list;
+}
+
+void InstanceMgr::get_load_metrics(LoadBalanceInfos* infos) {
+  std::shared_lock<std::shared_mutex> inst_lock(inst_mutex_);
+  std::shared_lock<std::shared_mutex> metric_lock(load_metric_mutex_);
+
+  for (auto name : infos->overlap_scores.instances) {
+    auto it = load_metrics_.find(name);
+    if (it == load_metrics_.end()) {
+      continue;
+    }
+    auto instance_it = instances_.find(name);
+    if (instance_it == instances_.end()) {
+      continue;
+    }
+
+    if (instance_it->second.type == InstanceType::DECODE) {
+      infos->decode_load_metrics.insert(std::make_pair(name, it->second));
+      infos->decode_max_waiting_requests_num =
+          std::max(infos->decode_max_waiting_requests_num,
+                   it->second.waiting_requests_num);
+    } else {
+      infos->prefill_load_metrics.insert(std::make_pair(name, it->second));
+      infos->prefill_max_waiting_requests_num =
+          std::max(infos->prefill_max_waiting_requests_num,
+                   it->second.waiting_requests_num);
+    }
+  }
+
+  std::string least_loaded_prefill_instance;
+  int32_t least_loaded_prefill_waiting_reqs = INT32_MAX;
+  std::string least_loaded_decode_instance;
+  int32_t least_loaded_decode_waiting_reqs = INT32_MAX;
+
+  if (infos->prefill_load_metrics.size() == 0 ||
+      infos->decode_load_metrics.size() == 0) {
+    for (const auto& metric : load_metrics_) {
+      auto instance_it = instances_.find(metric.first);
+      if (instance_it != instances_.end()) {
+        if (instance_it->second.type != InstanceType::DECODE) {
+          if (metric.second.waiting_requests_num <
+              least_loaded_prefill_waiting_reqs) {
+            least_loaded_prefill_waiting_reqs =
+                metric.second.waiting_requests_num;
+            least_loaded_prefill_instance = metric.first;
+          }
+        } else {
+          if (metric.second.waiting_requests_num <
+              least_loaded_decode_waiting_reqs) {
+            least_loaded_decode_waiting_reqs =
+                metric.second.waiting_requests_num;
+            least_loaded_decode_instance = metric.first;
+          }
+        }
+      }
+    }
+  }
+
+  if (infos->prefill_load_metrics.size() == 0 &&
+      !least_loaded_prefill_instance.empty()) {
+    infos->prefill_load_metrics.insert(
+        std::make_pair(least_loaded_prefill_instance,
+                       load_metrics_[least_loaded_prefill_instance]));
+    infos->prefill_max_waiting_requests_num = least_loaded_prefill_waiting_reqs;
+  }
+
+  if (infos->decode_load_metrics.size() == 0 &&
+      !least_loaded_decode_instance.empty()) {
+    infos->decode_load_metrics.insert(
+        std::make_pair(least_loaded_decode_instance,
+                       load_metrics_[least_loaded_decode_instance]));
+    infos->decode_max_waiting_requests_num = least_loaded_decode_waiting_reqs;
+  }
+}
+
+void InstanceMgr::record_load_metrics_update(
+    const std::string& instance_name,
+    const proto::LoadMetrics& load_metrics) {
+  std::lock_guard<std::mutex> lock(update_mutex_);
+
+  updated_metrics_.insert_or_assign(
+      instance_name,
+      LoadMetrics(load_metrics.waiting_requests_num(),
+                  load_metrics.gpu_cache_usage_perc()));
+}
+
+bool InstanceMgr::upload_load_metrics() {
+  std::lock_guard<std::mutex> lock(update_mutex_);
+  bool status = etcd_client_->set(ETCD_LOADMETRICS_PREFIX, updated_metrics_);
+  status =
+      status && etcd_client_->rm(ETCD_LOADMETRICS_PREFIX, removed_instance_);
+  {
+    std::unique_lock<std::shared_mutex> lock(inst_mutex_);
+    for (auto& iter : updated_metrics_) {
+      load_metrics_.insert_or_assign(iter.first, std::move(iter.second));
+    }
+    for (auto& iter : removed_instance_) {
+      load_metrics_.erase(iter);
+    }
+  }
+  updated_metrics_.clear();
+  removed_instance_.clear();
+
+  return status;
+}
+
+void InstanceMgr::set_as_master() {
+  is_master_service_ = true;
+  etcd_client_->remove_watch(ETCD_LOADMETRICS_PREFIX);
+}
+
+std::shared_ptr<brpc::Channel> InstanceMgr::get_channel(
+    const std::string& instance_name) {
+  std::shared_lock<std::shared_mutex> lock(inst_mutex_);
+  auto iter = cached_channels_.find(instance_name);
+  if (iter == cached_channels_.end()) {
+    return nullptr;
+  }
+  return iter->second;
+}
+
+bool InstanceMgr::create_channel(const std::string& instance_name) {
+  if (cached_channels_.find(instance_name) == cached_channels_.end()) {
+    auto channel = std::make_shared<brpc::Channel>();
+    brpc::ChannelOptions options;
+    // Add to params
+    options.protocol = "http";
+    options.timeout_ms = http_config_.timeout_ms; /*milliseconds*/
+    options.max_retry = 3;
+    std::string load_balancer = "";
+    if (channel->Init(instance_name.c_str(), load_balancer.c_str(), &options) !=
+        0) {
+      LOG(ERROR) << "Fail to initialize channel for " << instance_name;
+      return false;
+    }
+    cached_channels_[instance_name] = std::move(channel);
+  }
+
+  return true;
+}
+
+void InstanceMgr::handle_instance_metainfo_watch(const etcd::Response& response,
+                                                 const uint64_t& prefix_len) {
+  if (response.events().empty()) {
+    return;
+  }
+
+  std::unordered_map<std::string, InstanceMetaInfo> put_map;
+  std::vector<std::string> delete_list;
+
+  for (const auto& event : response.events()) {
+    std::string instance_name = event.kv().key().substr(prefix_len);
+
+    if (event.event_type() == etcd::Event::EventType::PUT) {
+      InstanceMetaInfo metainfo;
+      auto json_str = event.kv().as_string();
+      if (!metainfo.parse_from_json(json_str)) {
+        LOG(ERROR) << "pase json:" << json_str << " error!";
+        continue;
+      }
+
+      put_map.insert(std::make_pair(instance_name, std::move(metainfo)));
+
+    } else if (event.event_type() == etcd::Event::EventType::DELETE_) {
+      delete_list.push_back(instance_name);
+    }
+  }
+
+  {
+    std::unique_lock<std::shared_mutex> lock(inst_mutex_);
+    for (auto& iter : put_map) {
+      if (instances_.find(iter.first) != instances_.end()) {
+        LOG(ERROR) << "Instance is already registered, instance_name: "
+                   << iter.first;
+        continue;
+      }
+      instances_.insert(std::make_pair(iter.first, std::move(iter.second)));
+      create_channel(iter.first);
+    }
+
+    for (auto& iter : delete_list) {
+      if (instances_.find(iter) == instances_.end()) {
+        LOG(ERROR) << "Instance is already deleted, instance_name: " << iter;
+        continue;
+      }
+      // TODO: notify cache manager to clear expire cache
+      instances_.erase(iter);
+      cached_channels_.erase(iter);
+      {
+        std::lock_guard<std::mutex> lock(update_mutex_);
+        updated_metrics_.erase(iter);
+        removed_instance_.insert(iter);
+      }
+    }
+  }
+}
+
+void InstanceMgr::handle_load_metrics_watch(const etcd::Response& response,
+                                            const uint64_t prefix_len) {
+  if (response.events().empty()) {
+    return;
+  }
+
+  std::unordered_map<std::string, LoadMetrics> put_map;
+  std::vector<std::string> delete_list;
+
+  for (const auto& event : response.events()) {
+    std::string instance_name = event.kv().key().substr(prefix_len);
+
+    if (event.event_type() == etcd::Event::EventType::PUT) {
+      LoadMetrics load_metrics;
+      auto json_str = event.kv().as_string();
+      if (!load_metrics.parse_from_json(json_str)) {
+        LOG(ERROR) << "pase json:" << json_str << " error!";
+        continue;
+      }
+
+      put_map.insert(std::make_pair(instance_name, std::move(load_metrics)));
+
+    } else if (event.event_type() == etcd::Event::EventType::DELETE_) {
+      delete_list.push_back(instance_name);
+    }
+  }
+
+  {
+    std::unique_lock<std::shared_mutex> lock(load_metric_mutex_);
+    for (auto& iter : put_map) {
+      load_metrics_.insert_or_assign(iter.first, std::move(iter.second));
+    }
+
+    for (auto& iter : delete_list) {
+      load_metrics_.erase(iter);
+    }
+  }
 }
 
 }  // namespace xllm_service
