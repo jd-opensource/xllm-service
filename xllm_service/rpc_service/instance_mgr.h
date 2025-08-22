d44e3ac2c2d85f374fbee1f7362dfaa12ce3a510
kangmeng3 <kangmeng3@jd.com>
2025-08-22 16:13:50 +0800

    refactor: refactor InstanceMgr.

--- xllm_service/rpc_service/instance_mgr.h
+++ xllm_service/rpc_service/instance_mgr.h
@@ -15,58 +15,78 @@ limitations under the License.
 
 #pragma once
 
-#include <mutex>
+#include <brpc/channel.h>
+
+#include <shared_mutex>
 #include <thread>
 #include <unordered_map>
 #include <unordered_set>
 
+#include "common/macros.h"
 #include "common/types.h"
-#include "disagg_pd_policy.h"
 #include "etcd_client.h"
+#include "xllm_rpc_service.pb.h"
 
 namespace xllm_service {
 
-class InstanceMgr {
+class InstanceMgr final {
  public:
-  explicit InstanceMgr(const RpcServiceConfig& config);
+  explicit InstanceMgr(const std::shared_ptr<EtcdClient>& etcd_client,
+                       const HttpServiceConfig& config,
+                       const bool is_master_service);
+
   ~InstanceMgr();
-  ErrorCode heartbeat(const std::string& instance_name);
 
-  ErrorCode register_instance(const std::string& instance_name);
-  ErrorCode register_instance(const std::string& instance_name,
-                              const InstanceMetaInfo& metainfo);
-  ErrorCode update_instance_metainfo(const std::string& instance_name,
-                                     const InstanceMetaInfo& metainfo);
   InstanceMetaInfo get_instance_info(const std::string& instance_name);
-
-  // select instances(prefill/decode/default etc.) to handle request
-  // according the disagg pd policy (or some other policies.).
-  InstancesPair select_instances_pair(bool only_prefill = false);
 
   std::vector<std::string> get_static_decode_list(
       const std::string& instance_name);
 
- private:
-  void internal_init();
-  // save instance metainfo to etcd
-  void save_persistence_metainfo(const InstanceMetaInfo& metainfo);
-  // delete instance metainfo from etcd
-  void delete_persistence_metainfo(
-      const std::vector<std::string>& instance_names);
-  void detect_disconnected_instances();
-  void update_instance_timestamp(const std::string& inst_name);
+  void get_load_metrics(LoadBalanceInfos* infos);
+
+  std::shared_ptr<brpc::Channel> get_channel(const std::string& instance_name);
+
+  void record_load_metrics_update(const std::string& instance_name,
+                                  const proto::LoadMetrics& load_metrics);
+  bool upload_load_metrics();
+
+  void set_as_master();
 
  private:
-  RpcServiceConfig config_;
+  DISALLOW_COPY_AND_ASSIGN(InstanceMgr);
+
+  void init();
+
+  bool create_channel(const std::string& target_uri);
+  // use etcd as ServiceDiscovery
+  void handle_instance_metainfo_watch(const etcd::Response& response,
+                                      const uint64_t& prefix_len);
+
+  void handle_load_metrics_watch(const etcd::Response& response,
+                                 const uint64_t prefix_len);
+
+ private:
   bool exited_ = false;
-  std::mutex inst_mutex_;
-  std::unordered_map<std::string, InstanceMetaInfo> instances_;
-  std::unique_ptr<std::thread> heartbeat_thread_;
-
-  std::unique_ptr<DisaggPdPolicy> disagg_pd_policy_;
-
   bool use_etcd_ = false;
-  std::unique_ptr<EtcdClient> etcd_client_;
+  std::atomic_bool is_master_service_ = false;
+
+  RpcServiceConfig config_;
+  HttpServiceConfig http_config_;
+
+  std::shared_ptr<EtcdClient> etcd_client_;
+
+  std::shared_mutex inst_mutex_;
+  std::unordered_map<std::string, InstanceMetaInfo> instances_;
+
+  std::shared_mutex load_metric_mutex_;
+  std::unordered_map<std::string, LoadMetrics> load_metrics_;
+  std::unordered_map<std::string, std::shared_ptr<brpc::Channel>>
+      cached_channels_;
+  std::unordered_set<std::string> zombie_nodes_;
+
+  std::mutex update_mutex_;
+  std::unordered_map<std::string, LoadMetrics> updated_metrics_;
+  std::unordered_set<std::string> removed_instance_;
 };
 
 }  // namespace xllm_service
